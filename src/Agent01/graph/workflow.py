309737028cda5d382058e5d3f26e0f735e83d5dd
劉昕晟 LIU XINSHENG <103816369+Babe153@users.noreply.github.com>
2026-09-19 00:13:34 +1000

    feat: add workflow.py and draw the work graph for agent

--- src/Agent01/graph/workflow.py
+++ src/Agent01/graph/workflow.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
 
-from langgraph.graph import END, START, StateGraph
+from langgraph.graph import END, START, StateGraph #导入画图工具包
 
-from Agent01.graph.nodes import actor_node, final_node, planner_node, verifier_node, verifier_route
-from Agent01.graph.state import MokioGraphState
+from Agent01.graph.nodes import actor_node, final_node, planner_node, verifier_node, verifier_route #导入所有节点
+from Agent01.graph.state import Agent01GraphState
+
+def build_workflow():
+    graph = StateGraph(Agent01GraphState) #创建一个 LangGraph 工作流构建对象，并指定它的共享状态结构为 Agent01GraphState
+    graph.add_node("planner", planner_node)
+    graph.add_node("actor", actor_node)
+    graph.add_node("verifier", verifier_node)
+    graph.add_node("final", final_node)
+
+    graph.add_edge(START, "planner")
+    graph.add_edge("planner", "actor")
+    graph.add_edge("actor", "verifier")
+    graph.add_conditional_edges("verifier", verifier_route, {"planner": "planner", "final": "final"})
+    graph.add_edge("final", END)
+    return graph.compile()
